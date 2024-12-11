eaca1064c36a6602e0a804ef116031e2a5f6c1cf
Michael Gerits <michael.gerits@alchemich.be>
2024-12-11 17:12:06 +0100

    initialize the attachments

--- iteration.py
+++ iteration.py
@@ -1,6 +1,7 @@
 import Main
 import numpy as np
 import PartLib
+from coordinate_conversion import cylindrical_to_cartesian
 
 structuralCylinder = PartLib.StructuralCylinder(R=12) #TODO: update initial dimensions
 
@@ -8,6 +9,14 @@ if Main.eulerBuckling(structuralCylinder) > structuralCylinder.sigmacr:
     print("Fail")
 
 
-#allocates the initial transverse and closing panels as a list
+#allocates the initial lists
+#----------------------------------------------------------------------------------------------------------------------------------
 closePanelList = [PartLib.ClosingPanel()] * PartLib.closingpanelAmount #TODO: add initial dimensions
-transversePanelList = [PartLib.TransversePanel()] * PartLib.transversePanelAmount #TODO: add initial dimensions
+transversePanelList = [PartLib.TransversePanel()] * 2 #TODO: add initial dimensions
+Attachments = []
+angles = np.linspace(0,360,num=PartLib.AttachmentPerPlate) #equally space the attachments
+
+for i in range(PartLib.AttachmentPerPlate, step=2): #assign the positions
+    Attachments.append(PartLib.Attachment(pos=np.array([structuralCylinder.R, angles[i], 0]))) #lower plate
+    Attachments.append(PartLib.Attachment(pos=np.array([structuralCylinder.R, angles[i], PartLib.transversePanelHeight]))) #upper plate
+#-----------------------------------------------------------------------------------------------------------------------------------
