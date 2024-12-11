cf977a81f4a7f72dea2dc48fd7c06eaa8d1d28e6
mgm3004 <matheusguimamello@gmail.com>
2024-12-11 15:12:43 +0100

    euler buckling function

--- Main.py
+++ Main.py
@@ -4,3 +4,9 @@ import scipy
 import PartLib
 
 # Task 5.2 sizing primary structure for internal pressure.
+
+# Task 5.3 buckling
+
+def EulerBuckling(E, I, A, L):
+    critical_euler_stress = ((math.pi ** 2) * E * I) / A * (L**2)
+    return critical_euler_stress
