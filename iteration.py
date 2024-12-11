35e413ef72eac8769854207e5acae5a527cf6e07
SilverBirdie <anwitagadgil1@gmail.com>
2024-12-11 16:10:06 +0100

    did some things

--- iteration.py
+++ iteration.py
@@ -4,6 +4,10 @@ import PartLib
 
 structuralCylinder = PartLib.StructuralCylinder(R=12) #TODO: update initial dimensions
 
+if Main.eulerBuckling(structuralCylinder) > structuralCylinder.sigmacr:
+    print("Fail")
+
+
 #allocates the initial transverse and closing panels as a list
 closePanelList = [PartLib.ClosingPanel()] * PartLib.closingpanelAmount #TODO: add initial dimensions
 transversePanelList = [PartLib.TransversePanel()] * PartLib.transversePanelAmount #TODO: add initial dimensions
