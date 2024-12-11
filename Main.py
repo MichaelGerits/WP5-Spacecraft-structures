6c17c0936cfe590fd615f9843e92f4c234d4dc9d
mgm3004 <matheusguimamello@gmail.com>
2024-12-11 15:33:18 +0100

    task 5.3 function updates

--- Main.py
+++ Main.py
@@ -7,21 +7,44 @@ import PartLib
 
 # Task 5.3 buckling
 
-def eulerBuckling(E, I, A, L):
+def eulerBuckling(cylinder):
+    I = cylinder.calcInertia()
+    E = cylinder.E
+    A = cylinder.calcArea()
+    L = cylinder.h
+
     critical_euler_stress = ((math.pi ** 2) * E * I) / A * (L**2)
     return critical_euler_stress
 
-def bucklingK(L, R, t1, Poisson, half_waves):
-    buckling_k = half_waves + (12 * (L ** 4) * (1 - (Poisson ** 2)))/((math.pi ** 4) * (R ** 2) * (t1 ** 2) * half_waves)
+def bucklingK(cylinder):
+    L = cylinder.h
+    R = cylinder.R
+    t = cylinder.t
+    Poisson = cylinder.Poisson
+    half_waves = cylinder.walf_waves
+
+    buckling_k = half_waves + (12 * (L ** 4) * (1 - (Poisson ** 2)))/((math.pi ** 4) * (R ** 2) * (t ** 2) * half_waves)
     return buckling_k
 
-def bucklingQ(p, E, R, t1):
-    buckling_Q = (p/E) * (R/t1)**2
+def bucklingQ(cylinder):
+    p = cylinder.p
+    E = cylinder.E
+    R = cylinder.R
+    t = cylinder.t
+
+    buckling_Q = (p/E) * (R/t)**2
     return buckling_Q
 
-def shellBuckling(buckling_Q, buckling_k, E, Poisson, t1, L):
+def shellBuckling(cylinder):
+    E = cylinder.E
+    Poisson = cylinder.Poisson
+    t = cylinder.t
+    L = cylinder.h
+    buckling_Q = bucklingQ(cylinder)
+    buckling_k = bucklingK(cylinder)
+
     coefficient = 1.983 - 0.983*math.exp(-23.14*buckling_Q)
     ratio1 = ((math.pi **2)*E)/(12*(1 - (Poisson**2)))
-    ratio2 = (t1/L)**2
+    ratio2 = (t/L)**2
     critical_shell_stress = coefficient * buckling_k * ratio1 * ratio2
     return critical_shell_stress
