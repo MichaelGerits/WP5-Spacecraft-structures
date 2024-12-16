cab8cf8b388581d98e5933375710c1025c3dd20b
Michael Gerits <michael.gerits@alchemich.be>
2024-12-16 14:51:51 +0100

    added density value

--- PartLib.py
+++ PartLib.py
@@ -12,7 +12,7 @@ class StructuralCylinder:
     """
     holds geometry and properties of the structural cylinder
     """
-    def __init__(self, R=0.28, h=1.5, t=0, E=110.3e9, SigmaY=980e6, rho=0, critical_euler_stress=0, critical_shell_stress=0, Poisson=0.32, buckling_k=0, half_waves=0):
+    def __init__(self, R=0.28, h=1.5, t=0, E=110.3e9, SigmaY=980e6, rho=4540, critical_euler_stress=0, critical_shell_stress=0, Poisson=0.32, buckling_k=0, half_waves=0):
         self.R = R
         self.h = h
         self.t = t
@@ -26,7 +26,6 @@ class StructuralCylinder:
         self.inertia = self.calcInertia()
         self.mass = self.calcMass()
         self.buckling_k = buckling_k
-        self.internal_pressure = internal_pressure
         self.half_waves = half_waves
         pass
 
