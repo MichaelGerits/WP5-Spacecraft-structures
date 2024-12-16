8502c974e7d8ef57abc41a04b2ad58dff608417e
SilverBirdie <anwitagadgil1@gmail.com>
2024-12-16 14:38:10 +0100

    got lambda (but its questionable)

--- iteration.py
+++ iteration.py
@@ -11,7 +11,7 @@ if Main.eulerBuckling(structuralCylinder) < Loads.zstress:
     print("Fail")
 
 MichaelsBaneResultoOfLambda = optimize.minimize(Main.bucklingK, [structuralCylinder.h, structuralCylinder.R, structuralCylinder.t, structuralCylinder.Poisson, structuralCylinder.half_waves], bounds=optimize.Bounds([structuralCylinder.h, structuralCylinder.R, structuralCylinder.t, structuralCylinder.Poisson, 0], [structuralCylinder.h, structuralCylinder.R, structuralCylinder.t, structuralCylinder.Poisson, 100]))
-
+print(MichaelsBaneResultoOfLambda)
 #allocates the initial lists
 #----------------------------------------------------------------------------------------------------------------------------------
 closePanelList = [PartLib.ClosingPanel(w=1, h=1.5)] * PartLib.closingpanelAmount
@@ -19,7 +19,7 @@ transversePanelList = [PartLib.TransversePanel(R_outer=1)] * 2 #TODO: add initia
 Attachments = []
 angles = np.linspace(0,360,num=PartLib.AttachmentPerPlate) #equally space the attachments
 
-for i in range(PartLib.AttachmentPerPlate, step=2): #assign the positions
+for i in range(1, PartLib.AttachmentPerPlate, 2): #assign the positions
     Attachments.append(PartLib.Attachment(pos=np.array([structuralCylinder.R, angles[i], 0]))) #lower plate
     Attachments.append(PartLib.Attachment(pos=np.array([structuralCylinder.R, angles[i], PartLib.transversePanelHeight]))) #upper plate
 #-----------------------------------------------------------------------------------------------------------------------------------
