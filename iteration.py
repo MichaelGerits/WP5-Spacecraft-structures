efc106fadf3a2017283eb5ddafed5e46cd03b2c5
SilverBirdie <anwitagadgil1@gmail.com>
2025-01-06 14:02:25 +0100

    bookkeeping

--- iteration.py
+++ iteration.py
@@ -72,7 +72,7 @@ while massdiff >= 0.001:
     """
 
     #optimising for buckling (its scuffed)
-    Resulto_Buck = optimize.minimize(Main.Buck, [structuralCylinder.t], args=[structuralCylinder.E, structuralCylinder.Poisson, structuralCylinder.SigmaY, structuralCylinder.h, P[1], 0.28], bounds=optimize.Bounds([Main.constrainer(structuralCylinder, P[1])], [0.1]))
+    Resulto_Buck = optimize.minimize(Main.Buck, [structuralCylinder.t], args=[structuralCylinder.E, structuralCylinder.Poisson, structuralCylinder.SigmaY, structuralCylinder.h, P[1], structuralCylinder.R], bounds=optimize.Bounds([Main.constrainer(structuralCylinder, P[1])], [0.1]))
     print(Resulto_Buck) #prints the results of the optimiser
     structuralCylinder.mass = structuralCylinder.rho * Resulto_Buck.fun #the function output volume, so the mass is the output(.fun) times density
     # updates structuralCylinder with the values given by the optimiser
